58ce79fd6aa6fec22f3ac97aaf13a7a8e1f08acc
Vanesa Romero - GH <vaparova_92@hotmail.com>
2021-12-03 20:22:16 -0300

    6.3.3

--- src/app/pages/index/index.page.ts
+++ src/app/pages/index/index.page.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { DatosSesion } from 'src/app/models/datosSesion';
+import { DatosUsuario } from 'src/app/models/datosUsuario';
+import { UsuariosService } from '../../providers/usuarios.service';
+import { ToastsService } from '../../providers/toasts.service';
+import { NavController, AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-index',
@@ -7,9 +12,57 @@ import { Component, OnInit } from '@angular/core';
 })
 export class IndexPage implements OnInit {
 
-  constructor() { }
+  sesion: DatosSesion;
+  usuario: DatosUsuario;
+  nombre = 'string';
+
+  constructor(private user: UsuariosService, private toast: ToastsService,
+              private navCtrl: NavController, private alertController: AlertController) {
+    this.obtenerData();
+   }
 
   ngOnInit() {
+    this.nombre = this.usuario.usuario.datosPersonales.nombre + ' ' + this.usuario.usuario.datosPersonales.apellido;
   }
 
+  async salir() {
+    const alert = await this.alertController.create({
+      cssClass: 'my-custom-class',
+      header: 'Cerrar Sesión',
+      message: '¿Estas seguro de cerrar tu sesión?',
+      buttons: [
+        {
+          text: 'Cancelar',
+          role: 'cancel',
+          cssClass: 'secondary',
+          handler: (blah) => {
+            console.log('Confirm Cancel: blah');
+          }
+        }, {
+          text: 'Salir',
+          handler: () => {
+            console.log('Confirm Okay');
+            this.navCtrl.navigateBack('/ingreso');
+          }
+        }
+      ]
+    });
+
+    await alert.present();
+  }
+
+  private obtenerData(): void{
+    const a = this.user.validarSesion();
+    if (a){
+      this.sesion = a;
+      this.usuario = this.user.obtenerUsuario(this.sesion.cuil);
+      console.log(`respta obtenerUsuario() US: ${this.usuario}`);
+      console.log(this.usuario);
+    }else{
+      this.user.borrarSesion();
+      this.toast.mostrarToast('Debes iniciar sesión', 'danger');
+      this.navCtrl.navigateBack('/ingreso');
+      console.log('error de login!');
+    }
+  }
 }
